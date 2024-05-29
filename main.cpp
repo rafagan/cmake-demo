f77f46bfe4a4c48fb3f85899d7cfb14600ac54e0
Ráfagan Abreu <r.abreu@transferbank.com.br>
2024-05-29 16:36:45 -0300

    feat: macros

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+#ifdef FLAG_MACRO
+    cout << "FLAG_MACRO defined" << endl;
+#endif
+
+#ifdef VALUE_MACRO
+    cout << "VALUE_MACRO defined: " << VALUE_MACRO << endl;
+#endif
+
+#ifdef STRING_MACRO
+    cout << "STRING_MACRO defined: " << STRING_MACRO << endl;
+#endif
+
     return 0;
 }
